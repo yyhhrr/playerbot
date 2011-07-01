ce83fd5444f10e94a4b5496fa1f9f1ad04992f03
Sergey Kuzmin <sergey.kuzmin@artezio.com>
2011-07-01 09:16:04 +0400

    adction node factory reused in bear tank strategy

--- src/game/playerbot/strategy/druid/BearTankDruidStrategy.cpp
+++ src/game/playerbot/strategy/druid/BearTankDruidStrategy.cpp
@@ -5,6 +5,115 @@
 
 using namespace ai;
 
+class BearTankDruidStrategyActionNodeFactory : public NamedObjectFactory<ActionNode>
+{
+public:
+    BearTankDruidStrategyActionNodeFactory()
+    {
+        creators["melee"] = &melee;
+        creators["feral charge - bear"] = &feral_charge_bear;
+        creators["swipe (bear)"] = &swipe_bear;
+        creators["faerie fire (feral)"] = &faerie_fire_feral;
+        creators["bear form"] = &bear_form;
+        creators["dire bear form"] = &dire_bear_form;
+        creators["mangle (bear)"] = &mangle_bear;
+        creators["maul"] = &maul;
+        creators["bash"] = &bash;
+        creators["swipe"] = &swipe;
+        creators["demoralizing roar"] = &demoralizing_roar;
+    }
+private:
+    static ActionNode* melee(PlayerbotAI* ai)
+    {
+        return new ActionNode ("melee",
+            /*P*/ NextAction::array(0, new NextAction("dire bear form"), new NextAction("feral charge - bear"), NULL),
+            /*A*/ NULL,
+            /*C*/ NULL);
+    }
+    static ActionNode* feral_charge_bear(PlayerbotAI* ai)
+    {
+        return new ActionNode ("feral charge - bear",
+            /*P*/ NULL,
+            /*A*/ NextAction::array(0, new NextAction("reach melee"), NULL),
+            /*C*/ NULL);
+    }
+    static ActionNode* swipe_bear(PlayerbotAI* ai)
+    {
+        return new ActionNode ("swipe (bear)",
+            /*P*/ NextAction::array(0, new NextAction("dire bear form"), NULL),
+            /*A*/ NULL,
+            /*C*/ NULL);
+    }
+    static ActionNode* faerie_fire_feral(PlayerbotAI* ai)
+    {
+        return new ActionNode ("faerie fire (feral)",
+            /*P*/ NextAction::array(0, new NextAction("dire bear form"), new NextAction("feral charge - bear"), NULL),
+            /*A*/ NULL,
+            /*C*/ NextAction::array(0, new NextAction("melee", 10.0f), NULL));
+    }
+    static ActionNode* bear_form(PlayerbotAI* ai)
+    {
+        return new ActionNode ("bear form",
+            /*P*/ NULL,
+            /*A*/ NULL,
+            /*C*/ NextAction::array(0, new NextAction("melee", 10.0f), NULL));
+    }
+    static ActionNode* dire_bear_form(PlayerbotAI* ai)
+    {
+        return new ActionNode ("dire bear form",
+            /*P*/ NULL,
+            /*A*/ NULL,
+            /*C*/ NextAction::array(0, new NextAction("melee", 10.0f), NULL));
+    }
+    static ActionNode* mangle_bear(PlayerbotAI* ai)
+    {
+        return new ActionNode ("mangle (bear)",
+            /*P*/ NULL,
+            /*A*/ NextAction::array(0, new NextAction("maul"), NULL),
+            /*C*/ NextAction::array(0, new NextAction("melee", 10.0f), NULL));
+    }
+    static ActionNode* maul(PlayerbotAI* ai)
+    {
+        return new ActionNode ("maul",
+            /*P*/ NULL,
+            /*A*/ NextAction::array(0, new NextAction("melee"), NULL),
+            /*C*/ NextAction::array(0, new NextAction("melee", 10.0f), NULL));
+    }
+    static ActionNode* bash(PlayerbotAI* ai)
+    {
+        return new ActionNode ("bash",
+            /*P*/ NULL,
+            /*A*/ NextAction::array(0, new NextAction("melee"), NULL),
+            /*C*/ NextAction::array(0, new NextAction("melee", 10.0f), NULL));
+    }
+    static ActionNode* swipe(PlayerbotAI* ai)
+    {
+        return new ActionNode ("swipe",
+            /*P*/ NULL,
+            /*A*/ NextAction::array(0, new NextAction("melee"), NULL),
+            /*C*/ NextAction::array(0, new NextAction("melee", 10.0f), NULL));
+    }
+    static ActionNode* growl(PlayerbotAI* ai)
+    {
+        return new ActionNode ("growl",
+            /*P*/ NextAction::array(0, new NextAction("dire bear form"), new NextAction("reach spell"), NULL),
+            /*A*/ NULL,
+            /*C*/ NextAction::array(0, new NextAction("melee", 10.0f), NULL));
+    }
+    static ActionNode* demoralizing_roar(PlayerbotAI* ai)
+    {
+        return new ActionNode ("demoralizing roar",
+            /*P*/ NextAction::array(0, new NextAction("dire bear form"), NULL),
+            /*A*/ NULL,
+            /*C*/ NextAction::array(0, new NextAction("melee", 10.0f), NULL));
+    }
+};
+
+BearTankDruidStrategy::BearTankDruidStrategy(PlayerbotAI* ai) : FeralDruidStrategy(ai)
+{
+    actionNodeFactories.Add(new BearTankDruidStrategyActionNodeFactory());
+}
+
 NextAction** BearTankDruidStrategy::getDefaultActions()
 {
     return NextAction::array(0, new NextAction("melee", 10.0f), NULL);
@@ -45,99 +154,4 @@ void BearTankDruidStrategy::InitTriggers(std::list<TriggerNode*> &triggers)
     triggers.push_back(new TriggerNode(
         "faerie fire (feral)",
         NextAction::array(0, new NextAction("faerie fire (feral)", 30.0f), NULL)));
-}
-
-void BearTankDruidStrategy::InitMultipliers(std::list<Multiplier*> &multipliers)
-{
-
-}
-
-
-ActionNode* BearTankDruidStrategy::GetAction(string name)
-{
-    if (name == "melee")
-    {
-        return new ActionNode ("melee",
-            /*P*/ NextAction::array(0, new NextAction("dire bear form"), new NextAction("feral charge - bear"), NULL),
-            /*A*/ NULL,
-            /*C*/ NULL);
-    }
-    else if (name == "feral charge - bear")
-    {
-        return new ActionNode ("feral charge - bear",
-            /*P*/ NULL,
-            /*A*/ NextAction::array(0, new NextAction("reach melee"), NULL),
-            /*C*/ NULL);
-    }
-    else if (name == "swipe (bear)")
-    {
-        return new ActionNode ("swipe (bear)",
-            /*P*/ NextAction::array(0, new NextAction("dire bear form"), NULL),
-            /*A*/ NULL,
-            /*C*/ NULL);
-    }
-    else if (name == "faerie fire (feral)")
-    {
-        return new ActionNode ("faerie fire (feral)",
-			/*P*/ NextAction::array(0, new NextAction("dire bear form"), new NextAction("feral charge - bear"), NULL),
-            /*A*/ NULL,
-            /*C*/ NextAction::array(0, new NextAction("melee", 10.0f), NULL));
-    }
-    else if (name == "bear form")
-    {
-        return new ActionNode ("bear form",
-            /*P*/ NULL,
-            /*A*/ NULL,
-            /*C*/ NextAction::array(0, new NextAction("melee", 10.0f), NULL));
-    }
-    else if (name == "dire bear form")
-    {
-        return new ActionNode ("dire bear form",
-            /*P*/ NULL,
-            /*A*/ NULL,
-            /*C*/ NextAction::array(0, new NextAction("melee", 10.0f), NULL));
-    }
-    else if (name == "mangle (bear)")
-    {
-        return new ActionNode ("mangle (bear)",
-            /*P*/ NULL,
-            /*A*/ NextAction::array(0, new NextAction("maul"), NULL),
-            /*C*/ NextAction::array(0, new NextAction("melee", 10.0f), NULL));
-    }
-    else if (name == "maul")
-    {
-        return new ActionNode ("maul",
-            /*P*/ NULL,
-            /*A*/ NextAction::array(0, new NextAction("melee"), NULL),
-            /*C*/ NextAction::array(0, new NextAction("melee", 10.0f), NULL));
-    }
-    else if (name == "bash")
-    {
-        return new ActionNode ("bash",
-            /*P*/ NULL,
-            /*A*/ NextAction::array(0, new NextAction("melee"), NULL),
-            /*C*/ NextAction::array(0, new NextAction("melee", 10.0f), NULL));
-    }
-    else if (name == "swipe")
-    {
-        return new ActionNode ("swipe",
-            /*P*/ NULL,
-            /*A*/ NextAction::array(0, new NextAction("melee"), NULL),
-            /*C*/ NextAction::array(0, new NextAction("melee", 10.0f), NULL));
-    }
-    else if (name == "growl")
-    {
-        return new ActionNode ("growl",
-            /*P*/ NextAction::array(0, new NextAction("dire bear form"), new NextAction("reach spell"), NULL),
-            /*A*/ NULL,
-            /*C*/ NextAction::array(0, new NextAction("melee", 10.0f), NULL));
-    }
-    else if (name == "demoralizing roar")
-    {
-        return new ActionNode ("demoralizing roar",
-            /*P*/ NextAction::array(0, new NextAction("dire bear form"), NULL),
-            /*A*/ NULL,
-            /*C*/ NextAction::array(0, new NextAction("melee", 10.0f), NULL));
-    }
-    else return FeralDruidStrategy::GetAction(name);
 }
--- src/game/playerbot/strategy/druid/BearTankDruidStrategy.h
+++ src/game/playerbot/strategy/druid/BearTankDruidStrategy.h
@@ -7,13 +7,11 @@ namespace ai
     class BearTankDruidStrategy : public FeralDruidStrategy
     {
     public:
-        BearTankDruidStrategy(PlayerbotAI* ai) : FeralDruidStrategy(ai) {}
+        BearTankDruidStrategy(PlayerbotAI* ai);
 
     public:
         virtual void InitTriggers(std::list<TriggerNode*> &triggers);
-        virtual void InitMultipliers(std::list<Multiplier*> &multipliers);
         virtual string getName() { return "bear"; }
-        virtual ActionNode* GetAction(string name);
         virtual NextAction** getDefaultActions();
 		virtual StrategyType GetType() { return STRATEGY_TYPE_TANK; }
     };
