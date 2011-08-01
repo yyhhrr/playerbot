2ee12908ff0610eaa0c7615fda41e087ba9f92e7
Sergey Kuzmin <sergey.kuzmin@artezio.com>
2011-08-01 14:19:36 +0400

    c [item] fixed so c leather shows real item links not leatherx127

--- src/game/playerbot/strategy/actions/TellItemCountAction.cpp
+++ src/game/playerbot/strategy/actions/TellItemCountAction.cpp
@@ -7,27 +7,8 @@ using namespace ai;
 
 bool TellItemCountAction::Execute(Event event)
 {
-    
     string text = event.getParam();
-
-    uint32 quality = chat->parseItemQuality(text);
-    if (quality == MAX_ITEM_QUALITY)
-    {
-        uint8 count = AI_VALUE2(uint8, "item count", text);
-
-        if (!count)
-            return false;
-
-        ostringstream out;
-        out << text << "x" << (int)count;
-        ai->TellMaster(out.str());
-
-        return true;
-    }
-
-    FindItemsToTradeByQualityVisitor visitor(quality, 100);
-    IterateItems(&visitor);
-    list<Item*> found = visitor.GetResult();
+    list<Item*> found = parseItems(text);
     for (list<Item*>::iterator i = found.begin(); i != found.end(); i++)
         TellItem((*i)->GetProto(), (*i)->GetCount());
 
