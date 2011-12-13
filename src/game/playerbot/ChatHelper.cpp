95dff03ac564bc298e8bb073fb712e2fe857d88e
Sergey Kuzmin <sergey.kuzmin@artezio.com>
2011-12-13 15:24:03 +0400

    [ai playerbot] shorter money format

--- src/game/playerbot/ChatHelper.cpp
+++ src/game/playerbot/ChatHelper.cpp
@@ -99,9 +99,10 @@ string ChatHelper::formatMoney(uint32 copper)
     out << " ";
     if (gold > 0)
         out << gold <<  "|TInterface\\AddOns\\AtlasLoot\\Images\\gold:0|t ";
-    if (silver > 0)
+    if (silver > 0 && gold < 50)
         out << silver <<  "|TInterface\\AddOns\\AtlasLoot\\Images\\silver:0|t ";
-    out << copper <<  "|TInterface\\AddOns\\AtlasLoot\\Images\\bronze:0|t";
+	if (copper > 0 && gold < 10)
+		out << copper <<  "|TInterface\\AddOns\\AtlasLoot\\Images\\bronze:0|t";
 
     return out.str();
 }
