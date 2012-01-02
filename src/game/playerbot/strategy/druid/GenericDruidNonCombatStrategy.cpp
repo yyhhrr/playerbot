2287dc8bf818ad90d7344d3b0f5a06d00bc5bec6
Ike Erhascy <ike3@aloe.no-ip.biz>
2012-01-02 12:19:46 +0400

    [ai playerbot] druid non combat: allow dispel

--- src/game/playerbot/strategy/druid/GenericDruidNonCombatStrategy.cpp
+++ src/game/playerbot/strategy/druid/GenericDruidNonCombatStrategy.cpp
@@ -55,6 +55,14 @@ void GenericDruidNonCombatStrategy::InitTriggers(std::list<TriggerNode*> &trigge
         "mark of the wild on party",
         NextAction::array(0, new NextAction("mark of the wild on party", 11.0f), NULL)));
 
+    triggers.push_back(new TriggerNode(
+        "cure poison",
+        NextAction::array(0, new NextAction("abolish poison", 21.0f), NULL)));
+
+    triggers.push_back(new TriggerNode(
+        "party member cure poison",
+        NextAction::array(0, new NextAction("abolish poison on party", 20.0f), NULL)));
+
 	triggers.push_back(new TriggerNode(
 		"party member dead",
 		NextAction::array(0, new NextAction("revive", 22.0f), NULL)));
--- src/tests/DruidNonCombatTestCase.cpp
+++ src/tests/DruidNonCombatTestCase.cpp
@@ -12,6 +12,7 @@ class DruidNonCombatTestCase : public EngineTestBase
     CPPUNIT_TEST( buff );
     CPPUNIT_TEST( low_mana );
 	CPPUNIT_TEST( resurrect );
+	CPPUNIT_TEST( dispel );
     CPPUNIT_TEST_SUITE_END();
 
 public:
@@ -39,8 +40,8 @@ protected:
 		assertActions(">P:revive");
 	}
 
-    void low_mana()
-    {
+	void low_mana()
+	{
         set<uint8>("item count", "drink", 1);
         tickWithLowMana(5);
         tickWithLowMana(5);
@@ -49,6 +50,19 @@ protected:
         tickWithLowMana(5);
 
         assertActions(">S:innervate>S:drink>S:flee");
+    }
+
+    void dispel()
+    {
+        tickWithAuraToDispel(DISPEL_POISON);
+        tickWithAuraToDispel(DISPEL_POISON);
+
+        spellAvailable("abolish poison");
+        spellAvailable("cure poison");
+        tickWithPartyAuraToDispel(DISPEL_POISON);
+        tickWithPartyAuraToDispel(DISPEL_POISON);
+
+        assertActions(">S:abolish poison>S:cure poison>P:abolish poison on party>P:cure poison on party");
     }
 };
 
