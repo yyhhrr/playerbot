294f72ee81f00bde527a6ca5a734de6588467993
Ike Erhascy <ike3@aloe.no-ip.biz>
2012-01-03 14:09:11 +0400

    [ai playerbot] crash when master is being teleported is fixed

--- src/game/playerbot/strategy/actions/UseMeetingStoneAction.cpp
+++ src/game/playerbot/strategy/actions/UseMeetingStoneAction.cpp
@@ -2,6 +2,32 @@
 #include "../../playerbot.h"
 #include "UseMeetingStoneAction.h"
 
+#include "../../../GridNotifiers.h"
+#include "../../../GridNotifiersImpl.h"
+#include "../../../CellImpl.h"
+#include "../../PlayerbotAIConfig.h"
+
+namespace MaNGOS
+{
+    class GameObjectByGuidInRangeCheck
+    {
+    public:
+        GameObjectByGuidInRangeCheck(WorldObject const* obj, ObjectGuid guid, float range) : i_obj(obj), i_range(range), i_guid(guid) {}
+        WorldObject const& GetFocusObject() const { return *i_obj; }
+        bool operator()(GameObject* u)
+        {
+            if (u && i_obj->IsWithinDistInMap(u, i_range) && u->isSpawned() && u->GetGOInfo() && u->GetObjectGuid() == i_guid)
+                return true;
+
+            return false;
+        }
+    private:
+        WorldObject const* i_obj;
+        float i_range;
+        ObjectGuid i_guid;
+    };
+};
+
 bool UseMeetingStoneAction::Execute(Event event)
 {
     WorldPacket p(event.getPacket());
@@ -15,7 +41,26 @@ bool UseMeetingStoneAction::Execute(Event event)
     if (!master->GetSelectionGuid() && master->GetGroup() != bot->GetGroup())
         return false;
 
-    GameObject* gameObject = master->GetMap()->GetGameObject(guid);
+    if (master->IsBeingTeleported())
+        return false;
+
+    list<GameObject*> targets;
+
+    MaNGOS::GameObjectByGuidInRangeCheck u_check(master, guid, sPlayerbotAIConfig.sightDistance);
+    MaNGOS::GameObjectListSearcher<MaNGOS::GameObjectByGuidInRangeCheck> searcher(targets, u_check);
+    Cell::VisitAllObjects(master, searcher, sPlayerbotAIConfig.sightDistance);
+
+    GameObject* gameObject = NULL;
+    for(list<GameObject*>::iterator i = targets.begin(); i != targets.end(); i++)
+    {
+        GameObject* go = *i;
+        if (go && go->isSpawned())
+        {
+            gameObject = go;
+            break;
+        }
+    }
+
     if (!gameObject)
         return false;
 
