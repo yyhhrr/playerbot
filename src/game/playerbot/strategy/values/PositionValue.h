6c7546fc67ea5b4b3b8788e7424810796e568765
Ike Erhascy <ike3@aloe.no-ip.biz>
2012-01-09 14:42:57 +0400

    [ai playerbot] linux compile fix

--- src/game/playerbot/strategy/values/PositionValue.h
+++ src/game/playerbot/strategy/values/PositionValue.h
@@ -6,13 +6,13 @@ namespace ai
     class Position
     {
     public:
-        Position() : isset(false) {}
-        void Set(double x, double y, double z) { this->x = x; this->y = y; this->z = z; this->isset = true; }
-        void Reset() { isset = false; }
-        bool isSet() { return isset; }
+        Position() : valueSet(false) {}
+        void Set(double x, double y, double z) { this->x = x; this->y = y; this->z = z; this->valueSet = true; }
+        void Reset() { valueSet = false; }
+        bool isSet() { return valueSet; }
 
         double x, y, z;
-        bool isset;
+        bool valueSet;
     };
 
     class PositionValue : public ManualSetValue<Position&>, public Qualified
