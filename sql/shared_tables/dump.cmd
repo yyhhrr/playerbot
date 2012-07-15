d4b60113cb641c9283ca1f5831f54067d37b9fd3
Ike Erhascy <ike3@aloe.no-ip.biz>
2012-07-15 08:06:31 +0400

    [shared tables] removed from playerbot

--- sql/shared_tables/dump.cmd
+++
@@ -1,9 +0,0 @@
-mysqldump -uroot -p1 characters70 character_achievement character_achievement_progress character_queststatus character_queststatus_daily character_queststatus_monthly character_queststatus_weekly character_reputation character_social instance instance_reset > backup.sql
-mysql -uroot -p1 -e "drop database if exists characters_common"
-mysql -uroot -p1 -e "create database characters_common"
-mysql -uroot -p1 characters_common < backup.sql
-mysql -uroot -p1 characters_common < move.sql
-mysql -uroot -p1 characters20 < characters_common.sql
-mysql -uroot -p1 characters60 < characters_common.sql
-mysql -uroot -p1 characters70 < characters_common.sql
-mysql -uroot -p1 characters80 < characters_common.sql
