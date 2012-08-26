00f3e033ec1e89a520402f9bbfea15bb98698620
Ike Erhascy <ike3@aloe.no-ip.biz>
2012-08-26 18:27:42 +0400

    [ai playerbot] check level of inviter

--- src/game/playerbot/strategy/actions/AcceptInvitationAction.h
+++ src/game/playerbot/strategy/actions/AcceptInvitationAction.h
@@ -10,8 +10,6 @@ namespace ai
 
         virtual bool Execute(Event event)
         {
-            
-
             Group* grp = bot->GetGroupInvite();
             if (!grp)
                 return false;
@@ -19,6 +17,15 @@ namespace ai
             Player* inviter = sObjectMgr.GetPlayer(grp->GetLeaderGuid());
             if (!inviter)
                 return false;
+
+            if (inviter != master) {
+                return false;
+            }
+
+            if ((int)bot->getLevel() - (int)inviter->getLevel() > 5 && master->GetRandomPlayerbotMgr()->IsRandomBot(bot)) {
+                ai->TellMaster("You are too low-level to invite me");
+                return false;
+            }
 
             WorldPacket p;
             uint32 roles_mask = 0;
