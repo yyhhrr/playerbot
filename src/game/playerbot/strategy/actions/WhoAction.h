6e5acde48e4eb944d5726b287af24c64aa8c9f2a
ike3 <ike3@aloe.no-ip.biz>
2013-01-16 11:04:52 +0400

    [ai-129] Show tradeskills in who action

---
+++ src/game/playerbot/strategy/actions/WhoAction.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#include "../Action.h"
+#include "InventoryAction.h"
+
+namespace ai
+{
+    class WhoAction : public InventoryAction {
+    public:
+        WhoAction(PlayerbotAI* ai) : InventoryAction(ai, "who") {}
+
+    public:
+        virtual bool Execute(Event event);
+        static map<uint32, string> skills;
+    };
+
+}
